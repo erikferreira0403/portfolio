123df4bcc8848ad27d168425aa94578a43bbdf66
Erik Alves <erikalves.alu.lmb@gmail.com>
2023-06-25 13:12:41 -0300

    icon modal

--- src/app/components/shared/float-icon/float-icon.component.ts
+++ src/app/components/shared/float-icon/float-icon.component.ts
@@ -1,6 +1,10 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Inject, ViewChild } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { MatTooltip } from '@angular/material/tooltip';
+import {MatDialog, MatDialogRef, MatDialogModule, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { Dialog } from '@angular/cdk/dialog';
+
 
 @Component({
   selector: 'app-float-icon',
@@ -13,6 +17,10 @@ export class FloatIconComponent  implements AfterViewInit {
   @ViewChild('tooltip') tooltip!: MatTooltip;
   showTooltip = true;
   tooltipText = ' Redes Sociais';
+  textoCompleto= '';
+  title = '';
+  constructor(public dialog: MatDialog) {}
+
 ngAfterViewInit() {
     if (this.tooltip) {
       this.tooltip.show();
@@ -34,5 +42,43 @@ ngAfterViewInit() {
 
     }
   }
-
+openDialog(link: string, title:string): void {
+  this.dialog.open(SeuModalComponent, {
+    width: '200px',
+    height:` 100px`,
+    data: { link: link, title: title }
+  });
 }
+}
+
+
+
+
+  @Component({
+    selector: 'seu-modal',
+    template: `
+    <div class="container">
+      <h3>Deseja ir para o {{data.title}}?</h3>
+      <a [href]="data.link">
+      <button mat-button color="primary" >Sim</button>
+      </a>
+      <button mat-button color="primary"(click)="closeModal()">Não</button>
+    </div>
+    `,
+      standalone: true,
+
+     imports: [MatDialogModule, MatButtonModule],
+     styles: ['.container { padding: 10px }']
+
+
+  })
+  export class SeuModalComponent {
+
+    constructor(@Inject(MAT_DIALOG_DATA) public data: {link: string, title: string}, public dialog: MatDialog
+    ) {}
+
+    closeModal(): void {
+      this.dialog.closeAll();
+      // Lógica para fechar o modal, se necessário
+    }
+  }
--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -14,17 +14,10 @@ interface MeuObjeto {
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-
-
-
   @Input() videoUrl!: string;
   isModalOpen = false;
   textoCompleto!: string;
   dialogRef!: MatDialogRef<any>;
-
-  public MeuObjeto: any = {
-    description: '',
-  };
 
    videoList: MeuObjeto[] = [
     {src:'../../assets/Listagem.mp4', title: 'CRUD MVC ASP.NET', description : '  #CRUD, dessa vez em #ASP.NET MVC.  O #MVC (Model-View-Controller) é um padrão de arquitetura amplamente usado no ASP.NET. Ele divide a lógica de negócios, a apresentação dos dados e a interação do usuário em três componentes: modelo, visão e controlador.  O modelo manipula os dados e a lógica de negócios, a visão exibe os dados e o controlador lida com as solicitações do usuário e coordena a interação entre o modelo e a visão. Essa separação de responsabilidades facilita a manutenção, reuso e colaboração no desenvolvimento de aplicativos web.     '},
@@ -32,7 +25,7 @@ export class ProjectsComponent {
 
   constructor(private dialog: MatDialog) {}
 
-  openModal(video:any, title: string, description: string) {
+  openModal(video:string, title: string, description: string) {
    this.dialog.open(VideosComponent, {
     height: '400px',
     width: '600px',
@@ -43,14 +36,4 @@ export class ProjectsComponent {
       }
     });
   }
-
-  imgCollection: Array<object> = [
-    {
-      image: '../../assets/img/image1.jpg',
-      thumbImage: '../../assets/img/image1.jpg',
-      alt: 'Image 1',
-      title: 'Image 1',
-      width: '400px', height: '300px'
-    }
-];
 }
