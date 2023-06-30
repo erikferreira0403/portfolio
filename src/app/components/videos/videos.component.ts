4a6747a27241ea822633b48a3380f544e068a7ce
Erik Alves <erikalves.alu.lmb@gmail.com>
2023-06-30 18:36:23 -0300

    videos responsiviness

--- src/app/components/videos/videos.component.ts
+++ src/app/components/videos/videos.component.ts
@@ -1,14 +1,21 @@
-import { Component, Inject, Input, inject } from '@angular/core';
+import { Component, Inject, Input, OnInit, inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
   styleUrls: ['./videos.component.css']
 })
-export class VideosComponent {
+export class VideosComponent implements OnInit {
+
+  safeurl: any;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {src:string, title:any, description: any},  private sanitizer: DomSanitizer){}
+  ngOnInit(): void {
+   this.safeurl = this.sanitizer.bypassSecurityTrustResourceUrl(this.data.src)
+   }
 
 
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {src:any, title:any, description: any}){}
 }
