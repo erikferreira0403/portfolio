34e89ec08b580b8172d40cbf1385cb9fa680bf90
Erik Alves <erikalves.alu.lmb@gmail.com>
2023-07-04 20:27:08 -0300

    dot

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -24,11 +24,11 @@ export class ProjectsComponent implements OnInit {
 
    videoList: MeuObjeto[] = [
     {src:'https://www.youtube.com/embed/mVMz744Q0rg',
-    img: '../../../assets/img/crudmvc.jpg',
+    img: '/../../../assets/img/crudmvc.jpg',
      title: 'CRUD MVC ASP.NET',
       description : '  #CRUD, dessa vez em #ASP.NET MVC.  O #MVC (Model-View-Controller) é um padrão de arquitetura amplamente usado no ASP.NET. Ele divide a lógica de negócios, a apresentação dos dados e a interação do usuário em três componentes: modelo, visão e controlador.  O modelo manipula os dados e a lógica de negócios, a visão exibe os dados e o controlador lida com as solicitações do usuário e coordena a interação entre o modelo e a visão. Essa separação de responsabilidades facilita a manutenção, reuso e colaboração no desenvolvimento de aplicativos web.     '},
     {src:'https://www.youtube.com/embed/CJDM7JezsE8',
-    img:'../../../assets/img/starwars.jpg',
+    img:'/../../../assets/img/starwars.jpg',
      title: 'STARS WARS',
      description : 'Um frontend em Angular 14 consumindo a Star Wars API'}];
 //<iframe width="560" height="315" src="https://www.youtube.com/embed/CJDM7JezsE8?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
